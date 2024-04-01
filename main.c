30983c2bdd824472553967197f16ad50e864ebb4
Xiang <xiang.chen@opendeusto.es>
2024-04-01 18:54:12 +0200

    menu

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+1. crear test
+    1.1 tipo pregunta: (T o F), (a,b,c,d), (a,b,c,d *2T), pregunta corta(limite de x caracteres), pregunta abierta
+    1.2 continuar
+    1.3 finalizar
+2. hacer test
+    tests...
+    2.1 hacer test x
+    2.2 atras
+3. visualizar nota
+    tests...
+    3.1 seleccion del test
+    3.2 atras
+4. eliminar test
+    tests...
+    4.1 elimanr test x
+        4.1.1 confirmar test x?
+        4.1.2 cancelar
+    4.2 atras
+5. salir
+*/
