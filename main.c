d7d60dc707c6e0212afa321f52db0b189a04d630
Xiang <xiang.chen@opendeusto.es>
2024-04-01 19:00:45 +0200

    menu base modificado

--- main.c
+++ main.c
@@ -4,13 +4,18 @@
     1.2 continuar
     1.3 finalizar
 2. hacer test
-    tests...
+    tests:
+        1. test1
+        2. test2...
     2.1 hacer test x
+
     2.2 atras
 3. visualizar nota
-    tests...
-    3.1 seleccion del test
-    3.2 atras
+    tests:
+        test1 3/5
+        test2 8/10
+        ...
+
 4. eliminar test
     tests...
     4.1 elimanr test x
