48f6d6ea271286fa3dda8a58d064beea774483ec
Marco <62694810+imZesk@users.noreply.github.com>
2024-04-05 19:04:34 +0200

    Creadas las estructuras de pregunta y GrupoPreguntas

---
+++ GrupoPreguntas.h
@@ -0,0 +1,11 @@
+#ifndef GRUPO_PREGUNTAS_H
+#define GRUPO_PREGUNTAS_H
+#include "Pregunta.h"
+
+typedef struct grupoPreguntas{
+	Pregunta * arrPreguntas;
+	int tam;
+	int numU;
+}GrupoPreguntas;
+
+#endif // GRUPO_PREGUNTAS_H
---
+++ pregunta.h
@@ -0,0 +1,10 @@
+#ifndef PREGUNTA_H
+#define PREGUNTA_H
+
+typedef struct {
+    char *pregunta;
+    char *tipoPregunta;
+    char *respuesta;
+}Pregunta;
+
+#endif // PREGUNTA_H
