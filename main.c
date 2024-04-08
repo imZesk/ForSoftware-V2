65723a5be000c5d21f49ca1779a5f0149fa73b1a
Endika Fernandez <fernandez.endika@opendeusto.es>
2024-04-08 21:54:39 +0200

    caso 2 terminado

--- main.c
+++ main.c
@@ -4,22 +4,24 @@
 #include "GrupoPreguntas.h"
 #include "encuesta.h"
 
-
-static int callback(void *data, int argc, char **argv, char **azColName) {
-    for (int i = 0; i < argc; i++) {
+static int callback(void *data, int argc, char **argv, char **azColName)
+{
+    for (int i = 0; i < argc; i++)
+    {
         printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
     }
     printf("\n");
     return 0;
 }
 
+int main()
+{
 
-int main(){
-
-    FILE *archivo; // Declara un puntero a FILE
+    FILE *archivo;                         // Declara un puntero a FILE
     archivo = fopen("./lib/Log.txt", "w"); // Abre el archivo en modo escritura ("w")
 
-    if (archivo == NULL) { // Verifica si ocurrió algún error al abrir el archivo
+    if (archivo == NULL)
+    { // Verifica si ocurrió algún error al abrir el archivo
         printf("Error al abrir el archivo.\n");
         return 1;
     }
@@ -29,64 +31,84 @@ int main(){
     sqlite3 *DB;
     char *errMsg = 0;
 
+    // Abrimos la bd
 
-    //Abrimos la bd
-    
     int existe = sqlite3_open("./lib/Preguntas.db", &DB);
     fprintf(archivo, "Base de datos abierta. \n");
     // //Abrimos la bd
     // int existe = sqlite3_open("./lib/Preguntas.db", &DB);
 
     // //Confirmamos que se abre correctamente
-    if (existe != SQLITE_OK) {
-         printf("Error");
-         //logger con el error
-         fprintf(archivo, "Error al abrir la base de datos. \n");
-         return 1;
+    if (existe != SQLITE_OK)
+    {
+        printf("Error");
+        // logger con el error
+        fprintf(archivo, "Error al abrir la base de datos. \n");
+        return 1;
     }
-    char *sql1 = "SELECT nombre, tipo_pregunta, pregunta, respuesta FROM pregunta where nombre = %s;"; 
-    //sustituir la x por el parametro que pase el usuario y la "" por lo que pase el usuario
+    char sql1[1000]; // = "SELECT nombre, tipo_pregunta, pregunta, respuesta FROM pregunta where nombre = %s;";
+    // sustituir la x por el parametro que pase el usuario y la "" por lo que pase el usuario
     char *sql2 = "SELECT nombre, tipo_pregunta, pregunta, respuesta FROM pregunta;";
-    char sql3[1000]; //= "DELETE FROM pregunta WHERE nombre = %s;"; 
-    //sustituir la x por el parametro que pase el usuario y la "" por lo que pase el usuario
+    char sql3[1000]; //= "DELETE FROM pregunta WHERE nombre = %s;";
+    // sustituir la x por el parametro que pase el usuario y la "" por lo que pase el usuario
     char *sql4 = "SELECT nombre FROM pregunta;";
 
-    //Menu:
+    // Menu:
     int contador;
     GrupoPreguntas grupoTest;
     char opcion;
     char eliminar[100];
-    	do{
-		opcion = menuPrincipal();
-		switch(opcion){
-            case '1':
-                        grupoTest = reservarMemoria();
-                        contador = 0;
-                        while (contador < grupoTest.tam) {
-                            addPregunta(&grupoTest);
-                            contador++;
-                            }
-                        
-                        mostarPreguntas(grupoTest);
-                        crearEncuesta(grupoTest);
-                        fprintf(archivo, "Test creado. \n");
-                break;
+    char hacer[100];
+    do
+    {
+        opcion = menuPrincipal();
+        switch (opcion)
+        {
+        case '1':
+            grupoTest = reservarMemoria();
+            contador = 0;
+            while (contador < grupoTest.tam)
+            {
+                addPregunta(&grupoTest);
+                contador++;
+            }
 
-			case '2':  
-                        
-                        existe = sqlite3_exec(DB, sql1, callback, 0, &errMsg);
-    
-                        if (existe != SQLITE_OK) {
-                            fprintf(stderr, "Error en la consulta SQL: %s\n", errMsg);
-                            sqlite3_free(errMsg);
-                        }
-                        fprintf(archivo, "Test completado. \n");
-				break;
+            mostarPreguntas(grupoTest);
+            crearEncuesta(grupoTest);
+            fprintf(archivo, "Test creado. \n");
+            break;
 
-			case '3':   
-                        
+        case '2':
+            existe = sqlite3_exec(DB, sql4, callback, 0, &errMsg);
 
-                        existe = sqlite3_exec(DB, sql2, callback, 0, &errMsg);
+            if (existe != SQLITE_OK)
+            {
+                fprintf(stderr, "Error en la consulta SQL: %s\n", errMsg);
+                sqlite3_free(errMsg);
+            }
+            printf("Nombre del test a hacer: ");
+
+            fflush(stdout);
+            fflush(stdin);
+
+            scanf("%s", hacer);
+            printf("prueba\n");
+            sprintf(sql1, "SELECT nombre, tipo_pregunta, pregunta, respuesta FROM pregunta where nombre = %s;", hacer);
+            printf("prueba2\n");
+
+            existe = sqlite3_exec(DB, sql1, callback, 0, &errMsg);
+
+            if (existe != SQLITE_OK)
+            {
+                fprintf(stderr, "Error en la consulta SQL: %s\n", errMsg);
+                sqlite3_free(errMsg);
+            }
+            fprintf(archivo, "Test completado. \n");
+            break;
+
+        case '3':
+
+            existe = sqlite3_exec(DB, sql2, callback, 0, &errMsg);
 
             if (existe != SQLITE_OK)
             {
@@ -99,14 +121,15 @@ int main(){
         case '4':
             existe = sqlite3_exec(DB, sql4, callback, 0, &errMsg);
 
-                if (existe != SQLITE_OK) {
+            if (existe != SQLITE_OK)
+            {
                 fprintf(stderr, "Error en la consulta SQL: %s\n", errMsg);
                 sqlite3_free(errMsg);
             }
-	        printf("Nombre del test a eliminar: ");
+            printf("Nombre del test a eliminar: ");
 
-	        fflush(stdout);
-	        fflush(stdin);
+            fflush(stdout);
+            fflush(stdin);
 
             scanf("%s", eliminar);
             printf("prueba\n");
@@ -139,5 +162,4 @@ int main(){
     sqlite3_close(DB);
     fprintf(archivo, "Base de datos cerrada. \n");
     fclose(archivo);
-
 }
