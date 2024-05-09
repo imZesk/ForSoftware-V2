accdafeabc98ebcaa9859a874a98fee257fecc00
AlexF093 <145749240+AlexF093@users.noreply.github.com>
2024-05-09 11:18:29 +0200

    cambios

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,5 +1,25 @@
+#include "Server.h"
+#include <iostream>
+#include <string>
+#include <sqlite3.h>
 
-
-int main(){
-
+using namespace std;
+namespace containers{
+    void BD::DB(){
+        
+    }
+    int BD::conectar(sqlite3* BD){
+        int existe = sqlite3_open("./lib/servidor.bd",& BD);
+        if (existe!=SQLITE_OK){
+            cout<<"Error al abrir la base de datos"<<endl;
+        }
+        return existe;
+    };
+    void BD::desconectar(sqlite3* BD){
+        sqlite3_close(BD);
+    };
+    void BD::visualizarTest(int existe){
+        char *sql = "Select nombre from test;";
+        
+    }
 }
